d425c2feed05459da3f3afc0ebeec0f5c1a1c4f4
beky <91580555+rahmatsubeki@users.noreply.github.com>
2024-02-09 19:59:50 +0700

    Update blog.js

--- assets/blog.js
+++ assets/blog.js
@@ -1,11 +1,9 @@
 function toggleCard(cardId) {
   var loginCard = document.getElementById('login');
-  var daftarCard = document.getElementById('daftar');
   var bannerCard = document.getElementById('banner');
 
   if (cardId === 'login') {
       bannerCard.style.display = 'none';
-      daftarCard.style.display = 'none';
       loginCard.style.display = 'block';
   } else {
       loginCard.style.display = 'none';
